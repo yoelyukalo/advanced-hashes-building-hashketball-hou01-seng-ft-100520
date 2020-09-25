2c3d563cfe00060aa40feff30465acaa7f7c40fe
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-25 02:32:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -72,7 +72,7 @@ def game_hash
           blocks: 7,
           slam_dunks: 2
         }, {
-          player_name: "Bismak Biyombo",
+          player_name: "Bismack Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
