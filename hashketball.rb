d392e779f9cd5c15e4a54f777d3a2c8495a8ef28
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-25 01:37:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,58 +1,59 @@
 def game_hash
-  home => {
-    team_name: "Brooklyn Nets",
-    colors: ["Black", "White"],
-    players: [
-      "Alan Anderson" => {
-      number: 0,
-      shoe: 16,
-      points: 22,
-      rebound: 12,
-      assists: 12,
-      steals: 3,
-      blocks: 1,
-      slam_dunks: 1
-    },
-      "Reggie Evans" => {
-      number: 30,
-      shoe: 14,
-      points: 12,
-      rebound: 12,
-      assists: 12,
-      steals: 12,
-      blocks: 12,
-      slam_dunks: 7
-    },
-      "Brook Lopez" => {
-      number: 11,
-      shoe: 17,
-      points: 17,
-      rebound: 19,
-      assists: 10,
-      steals: 3,
-      blocks: 1,
-      slam_dunks: 15
-    },
-      "Mason Plumlee" => {
-      number: 1,
-      shoe: 19,
-      points: 26,
-      rebound: 11,
-      assists: 6,
-      steals: 3,
-      blocks: 8,
-      slam_dunks: 5
-    },
-      "Jason Terry" => {
-      number: 31,
-      shoe: 15,
-      points: 19,
-      rebound: 2,
-      assists: 2,
-      steals: 4,
-      blocks: 11,
-      slam_dunks: 1
-    }
+  {
+    home => {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        "Alan Anderson" => {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebound: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        "Reggie Evans" => {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebound: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        "Brook Lopez" => {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebound: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+        "Mason Plumlee" => {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebound: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        "Jason Terry" => {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebound: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
     ]
   }
   away: {
