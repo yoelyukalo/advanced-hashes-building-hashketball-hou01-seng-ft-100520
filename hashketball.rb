c2a209e97ad2525bf341519f569f13455a44704a
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-25 02:32:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -78,7 +78,7 @@ def game_hash
           points: 12,
           rebounds: 4,
           assists: 7,
-          steals: 7,
+          steals: 22,
           blocks: 15,
           slam_dunks: 10
         }, {
