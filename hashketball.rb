f0aa2c3037d872718d873d94fbaff66ca0f8e83a
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-25 01:43:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -57,7 +57,7 @@ def game_hash
       ]
   },
   away: {
-    team_name: {"Charlotte Hornets"},
+    team_name: "Charlotte Hornets",
     colors: ["Turquoise", "Purple"],
     players: {
         "Jeff Adrien" => {
