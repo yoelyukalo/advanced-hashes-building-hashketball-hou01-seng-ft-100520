8699265480d3a21abbc8a53eb7bbea8e93d5aae4
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-25 01:09:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -99,15 +99,15 @@ def game_hash
       blocks:1,
       slam_dunks:0
     },
-      "" => {
-      number:0,
-      shoe:16,
-      points:22,
+      "Kemba Walker" => {
+      number:33,
+      shoe:15,
+      points:6,
       rebound:12,
       assists:12,
-      steals:3,
-      blocks:1,
-      slam_dunks:1
+      steals:7,
+      blocks:5,
+      slam_dunks:12
     },
     }
   },
