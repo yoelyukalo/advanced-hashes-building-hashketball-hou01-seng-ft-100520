55d7c9c86da69b56fe758bfae8771c08d649db55
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-25 01:49:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -56,68 +56,6 @@ def game_hash
         }
       ]
   },
-  away: 
-  {
-    team_name: "Charlotte Hornets",
-    colors: ["Turquoise", "Purple"],
-    players: 
-    [
-      {
-        "Jeff Adrien" => {
-      number: 4,
-      shoe: 18,
-      points: 10,
-      rebound: 1,
-      assists: 1,
-      steals: 2,
-      blocks: 7,
-      slam_dunks: 2
-    },
-      "Bismack Biyombo" => {
-      number: 0,
-      shoe: 16,
-      points: 12,
-      rebound: 4,
-      assists: 7,
-      steals: 22,
-      blocks: 15,
-      slam_dunks: 10
-    },
-      "DeSagna Diop" => {
-      number: 2,
-      shoe: 14,
-      points: 24,
-      rebound: 12,
-      assists: 12,
-      steals: 4,
-      blocks: 5,
-      slam_dunks: 5
-    },
-      "Ben Gordon" => {
-      number: 8,
-      shoe: 15,
-      points: 33,
-      rebound: 3,
-      assists: 2,
-      steals: 1,
-      blocks: 1,
-      slam_dunks: 0
-    },
-      "Kemba Walker" => {
-      number: 33,
-      shoe: 15,
-      points: 6,
-      rebound: 12,
-      assists: 12,
-      steals: 7,
-      blocks: 5,
-      slam_dunks: 12
-    },
-      }
-    ]
-  }
-end
-
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
